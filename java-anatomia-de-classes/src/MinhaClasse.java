d59bc237b3d29fb78a449f50473da8d0362cff3b
Rodrigo <chocoliris@msn.com>
2025-02-04 10:09:47 -0300

    chamado de um método

--- java-anatomia-de-classes/src/MinhaClasse.java
+++ java-anatomia-de-classes/src/MinhaClasse.java
@@ -1,7 +1,17 @@
+
+
 public class MinhaClasse {
 
     public static void main(String [] args){
-        System.out.print("Olá turma, sejam bem vindos.");
+        String primeiroNome = "Rodrigo";
+        String segundoNome = "Jesus";
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    }
+    
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return primeiroNome.concat(" ").concat(segundoNome); 
+
     }
     
 }
