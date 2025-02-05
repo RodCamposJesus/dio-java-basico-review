232f7e1ef31725ca69237ffb7d69d0ef61f7131e
Rodrigo <chocoliris@msn.com>
2025-02-05 17:06:50 -0300

    teste operadores

---
+++ java-anatomia-de-classes/src/edu/rodrigo/segundasemana/Operadores.java
@@ -0,0 +1,12 @@
+package edu.rodrigo.segundasemana;
+
+public class Operadores {
+    
+    public static void main(String[] args) {
+
+        String nomeCompleto = "LINGUAGEM" + " JAVA";
+        System.out.println(nomeCompleto);
+    }
+        
+    }
+   
